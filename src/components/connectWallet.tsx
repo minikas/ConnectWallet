25b48ff6baedc6f739f62d306aca9144fa82bf95
Kas <kas_pintxuki@outlook.com>
2024-09-25 16:08:14 -0300

    feat: connect wallet component

---
+++ src/components/connectWallet.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { WalletCard } from "./walletCard";
+
+export function ConnectWallet() {
+  return (
+    <section className="flex flex-col gap-6 hover:bg-muted/30 bg-muted/20 duration-300 transition-colors border rounded-lg p-6 h-fit w-full">
+      <header className="flex flex-col gap-1 items-center">
+        <h3 className="font-bold text-xl">Connect Wallet</h3>
+        <span className="text-sm text-muted-foreground">
+          Select what wallet you want to connect
+        </span>
+      </header>
+      <div className="flex flex-col gap-2">
+        <WalletCard icon="PolkadotJS" active>
+          PolkadotJS
+        </WalletCard>
+        <WalletCard icon="Talisman" active>
+          Talisman
+        </WalletCard>
+        <WalletCard icon="SubWallet">SubWallet</WalletCard>
+        <WalletCard icon="Enkrypt">Enkrypt</WalletCard>
+      </div>
+    </section>
+  );
+}
