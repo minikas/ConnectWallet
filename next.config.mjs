6aff83223bcd76ec21a287921107b83bafc7d912
Kas <kas_pintxuki@outlook.com>
2024-10-07 11:53:35 -0300

    fix: nextjs redirect paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  redirects: async () => {
+    return [
+      {
+        source: "/default/:path*",
+        destination: "/r/styles/default/:path*.json",
+        permanent: true,
+      },
+      {
+        source: "/new-york/:path*",
+        destination: "/r/styles/new-york/:path*.json",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
