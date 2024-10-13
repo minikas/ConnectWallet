2f7a4ddc1e3bcaea1e03599a90941f1e493aeba6
Kas <kas_pintxuki@outlook.com>
2024-10-13 10:32:36 -0300

    fix: metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Screen from "./screen";
 export const metadata: Metadata = {
   title: "Connect Wallet Demo",
   description:
-    "A Next.js application demonstrating a 'Connect Wallet' component UI for Substrate-based wallets such as Polkadot.js and Talisman.",
+    "A Next.js application demonstrating a Connect Wallet component UI for Substrate-based wallets such as Polkadot.js and Talisman",
 };
 
 export default function Home() {
